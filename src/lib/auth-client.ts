4c6fe351db9c2b09bb0b7a3f1f3e625cb1b8a49d
Luciano Orfino <luchoorfino@gmail.com>
2025-03-09 04:52:18 -0300

    fix production url breaking auth

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -2,7 +2,7 @@ import { createAuthClient } from "better-auth/react";
 
 const isDev = process.env.NEXT_PUBLIC_ENV === "development";
 const authClient = createAuthClient({
-    baseURL: isDev ? "http://localhost:3000" : "https://api.tcg-arg.com",
+    baseURL: isDev ? "http://localhost:3000" : "https://tcg-arg.vercel.app/",
 })
 
 export const {
